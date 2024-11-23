86c9b9bbf33d86dd0284950ead48adcc67cf76a0
Ankit Singh <2808aksingh@gmail.com>
2024-11-24 00:45:48 +0530

    [ADD] magicalarena: added folder structure, player class, magicalarena class
    - Added basic folder structure
    - Added player class with basic implementation
    - Added Magicalarena class with basic implementation

---
+++ src/main/java/MagicalArena.java
@@ -0,0 +1,7 @@
+public class MagicalArena {
+    public static void main(String[] args) {
+        Player playerA = new Player("Player A", 70, 5, 10);
+        Player playerB = new Player("Player B", 100, 10, 15);
+        System.out.println(playerB.getAttack());
+    }
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,29 @@
+public class Player {
+    private String name;
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(String name, int health, int strength, int attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+}
